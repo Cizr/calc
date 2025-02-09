62cd2d3d62f0c256fd38c3a86c85c54bc03cd36b
Benkaddour Racim <100844208+Cizr@users.noreply.github.com>
2025-02-09 11:53:13 +0100

    Update calculator.py (another case run)

--- calculator.py
+++ calculator.py
@@ -2,10 +2,10 @@ def add(x, y):
      return x + y + 1 #delebrated (the pipeline will fail because test_add will fail (2 + 3 should be 5, but it returns 6))
 
 def subtract(x, y):
-    return x - y
+    return x - y +2
 
 def multiply(x, y):
-    return x * y
+    return x * y +3
 
 def divide(x, y):
     if y == 0:
